d1fae937878dafb97fc1751007f2be5ae49b637f
Лодыгин Кирилл Валерьевич <lodygin@vtb.ru>
2025-01-29 14:49:06 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+const objectsGroups = [
+    [
+        { imgSrc: 'assets/images/wine.png', imgClass: 'wine', imgAlt: 'wine',  marginLeft: '20px'},
+        { imgSrc: 'assets/images/milk.png', imgClass: 'milk', imgAlt: 'milk',  marginLeft: '7px'},
+        { imgSrc: 'assets/images/jam.png', imgClass: 'jam', imgAlt: 'jam', marginLeft: '7px'},
+        { imgSrc: 'assets/images/cheese.png', imgClass: 'cheese', imgAlt: 'cheese', marginLeft: '7px' }
+    ],
+
+    [
+        { imgSrc: 'assets/images/meat.png', imgClass: 'meat', imgAlt: 'meat',  marginLeft: '10px'},
+        { imgSrc: 'assets/images/chicken.png', imgClass: 'chicken', imgAlt: 'chicken',  marginLeft: '3px'},
+        { imgSrc: 'assets/images/chips.png', imgClass: 'chips', imgAlt: 'chips', marginLeft: '3px'},
+    ],
+
+    [
+        { imgSrc: 'assets/images/pineapple.png', imgClass: 'pineapple', imgAlt: 'pineapple',  marginLeft: '15px'},
+        { imgSrc: 'assets/images/bananas.png', imgClass: 'bananas', imgAlt: 'bananas',  marginLeft: '7px'},
+        { imgSrc: 'assets/images/apple.png', imgClass: 'apple', imgAlt: 'apple', marginLeft: '9px'},
+        { imgSrc: 'assets/images/salad.png', imgClass: 'salad', imgAlt: 'salad', marginLeft: '8px' }
+    ]
+]
+
 function disableDragAndDrop() {
     const objects = document.querySelectorAll('.object');
     objects.forEach((object) => {
@@ -11,147 +33,128 @@ function disableDragAndDrop() {
 }
 
 function createPlaceholderForMovedElement(movedObject) {
-    // Создаем новый пустой div
     const placeholder = document.createElement('div');
     placeholder.className = 'placeholder';
 
-    // Копируем размеры и отступы перемещенного элемента
     const movedObjectStyles = getComputedStyle(movedObject);
     placeholder.style.width = movedObjectStyles.width;
     placeholder.style.marginLeft = movedObjectStyles.marginLeft;
     placeholder.style.marginTop = movedObjectStyles.marginTop;
 
-    // return movedObjectParentNode.insertBefore(placeholder, movedObject.nextSibling);
     return placeholder;
 }
 
-function createDynamicHtml() {
-    const objectsGroups = [
-        [
-            { imgSrc: 'assets/images/wine.png', imgClass: 'wine', imgAlt: 'wine',  marginLeft: '20px'},
-            { imgSrc: 'assets/images/milk.png', imgClass: 'milk', imgAlt: 'milk',  marginLeft: '7px'},
-            { imgSrc: 'assets/images/jam.png', imgClass: 'jam', imgAlt: 'jam', marginLeft: '7px'},
-            { imgSrc: 'assets/images/cheese.png', imgClass: 'cheese', imgAlt: 'cheese', marginLeft: '7px' }
-        ],
+function createNewContentContainer(objects, index) {
+    // Создаем контейнер для объектов
+    const container = document.createElement('div');
+    // Задаем css правила для разных полок
+    container.className = 'container';
+    if (index === 0) {
+        container.style.alignItems = 'flex-end';
+        container.style.top = '0';
+    }
+    if (index === 1) {
+        container.style.alignItems = 'flex-start';
+        container.style.top = '10px';
+    }
+    if (index === 2) {
+        container.style.alignItems = 'flex-end';
+        container.style.top = '25px';
+    }
 
-        [
-            { imgSrc: 'assets/images/meat.png', imgClass: 'meat', imgAlt: 'meat',  marginLeft: '10px'},
-            { imgSrc: 'assets/images/chicken.png', imgClass: 'chicken', imgAlt: 'chicken',  marginLeft: '3px'},
-            { imgSrc: 'assets/images/chips.png', imgClass: 'chips', imgAlt: 'chips', marginLeft: '3px'},
-        ],
+    let maxHeight = 0; // Переменная для хранения максимальной высоты элемента
+    let imagesLoadedCount = 0; // Счетчик загруженных изображений
 
-        [
-            { imgSrc: 'assets/images/pineapple.png', imgClass: 'pineapple', imgAlt: 'pineapple',  marginLeft: '15px'},
-            { imgSrc: 'assets/images/bananas.png', imgClass: 'bananas', imgAlt: 'bananas',  marginLeft: '7px'},
-            { imgSrc: 'assets/images/apple.png', imgClass: 'apple', imgAlt: 'apple', marginLeft: '9px'},
-            { imgSrc: 'assets/images/salad.png', imgClass: 'salad', imgAlt: 'salad', marginLeft: '8px' }
-        ]
-    ]
+    // Проходимся по массиву объектов и создаем соответствующий HTML
+    objects.forEach((object) => {
+        const objectDiv = document.createElement('div');
+        objectDiv.className = 'object';
+        const img = document.createElement('img');
+        img.setAttribute('loading', 'lazy');
+        img.src = object.imgSrc;
+        img.alt = object.imgAlt;
+        img.classList.add(object.imgClass);
+        img.classList.add('mouse-event-allowed')
 
-    const productContainer = document.querySelector('.product-container')
-    objectsGroups.forEach((objects, index) => {
-        // Создаем контейнер для объектов
-        const container = document.createElement('div');
-        // Задаем css правила для разных полок
-        container.className = 'container';
-        if (index === 0) {
-            container.style.alignItems = 'flex-end';
-            container.style.top = '0';
-        }
-        if (index === 1) {
-            container.style.alignItems = 'flex-start';
-            container.style.top = '10px';
-        }
-        if (index === 2) {
-            container.style.alignItems = 'flex-end';
-            container.style.top = '25px';
-        }
-        const sectionDivider = document.createElement('img');
-        sectionDivider.className = index < 2 ? 'section-divider' : 'footer-image';
-        sectionDivider.alt = 'footer-image';
-        sectionDivider.src = index < 2 ? 'assets/images/section-divider.svg' : 'assets/images/footer-image.svg';
+        // Устанавливаем стили для каждого объекта
+        objectDiv.style.marginLeft = object.marginLeft;
+        objectDiv.style.padding = 0;
+        objectDiv.classList.add('mouse-event-allowed')
 
-        const highlightBar = document.createElement('div');
-        highlightBar.className = index < 2 ? 'highlight-bar' : 'gray-separator';
+        objectDiv.appendChild(img);
+        container.appendChild(objectDiv);
 
-        const section = document.createElement('div');
-        section.className = 'section'
+        let parentNode = null;
 
-        section.appendChild(container)
-        section.appendChild(sectionDivider)
-        section.appendChild(highlightBar)
-        if (index < 2) {
-            const darkBar = document.createElement('div');
-            darkBar.className = 'dark-bar';
-            section.appendChild(darkBar)
-        }
-        // Позиционируем полку
-        if (index === 0) {
-            section.style.top = '0';
-        }
-        if (index === 1) {
-            section.style.top = '150px';
-        }
-        if (index === 2) {
-            section.style.top = '225px';
-        }
-
-        let maxHeight = 0; // Переменная для хранения максимальной высоты элемента
-        let imagesLoadedCount = 0; // Счетчик загруженных изображений
-
-        // Проходимся по массиву объектов и создаем соответствующий HTML
-        objects.forEach((object) => {
-            const objectDiv = document.createElement('div');
-            objectDiv.className = 'object';
-            const img = document.createElement('img');
-            img.setAttribute('loading', 'lazy');
-            img.src = object.imgSrc;
-            img.alt = object.imgAlt;
-            img.classList.add(object.imgClass);
-            img.classList.add('mouse-event-allowed')
-
-            // Устанавливаем стили для каждого объекта
-            objectDiv.style.marginLeft = object.marginLeft;
-            objectDiv.style.padding = 0;
-            objectDiv.classList.add('mouse-event-allowed')
-
-            objectDiv.appendChild(img);
-            container.appendChild(objectDiv);
-            let parentNode = null;
-
-            // Добавляем функциональность Drag & Drop
-            img.addEventListener('dragstart', (event) => {
-                parentNode = event.target.parentNode
-                parentNode.insertBefore(createPlaceholderForMovedElement(event.target), event.target)
-                parentNode.style.opacity = 0
-                event.dataTransfer.effectAllowed = 'move';
-                event.dataTransfer.setData('text/plain', object.imgClass);
-            });
-
-            img.addEventListener('dragend', (event) => {
-                parentNode.style.opacity = 1
-            })
-
-            // Ждем загрузки изображения
-            img.onload = () => {
-                imagesLoadedCount++;
-                // Определяем максимальную высоту элемента
-                const computedStyle = window.getComputedStyle(img);
-                const heightString = computedStyle.height.replace('px', '');
-                const height = parseInt(heightString);
-                if (height > maxHeight) {
-                    maxHeight = height;
-                }
-                // Если все изображения загружены, устанавливаем высоту контейнера
-                if (imagesLoadedCount === objects.length) {
-                    container.style.height = `${maxHeight}px`;
-                }
-            };
+        // Добавляем функциональность Drag & Drop
+        img.addEventListener('dragstart', (event) => {
+            parentNode = event.target.parentNode
+            parentNode.insertBefore(createPlaceholderForMovedElement(event.target), event.target)
+            parentNode.style.opacity = 0
+            event.dataTransfer.effectAllowed = 'move';
+            event.dataTransfer.setData('text/plain', object.imgClass);
         });
-        // Вставляем созданный контейнер в тело документа
-        productContainer.appendChild(section);
-    })
 
+        img.addEventListener('dragend', (event) => {
+            parentNode.style.opacity = 1
+        })
+
+        // Ждем загрузки изображения
+        img.onload = () => {
+            imagesLoadedCount++;
+            // Определяем максимальную высоту элемента
+            const computedStyle = window.getComputedStyle(img);
+            const heightString = computedStyle.height.replace('px', '');
+            const height = parseInt(heightString);
+            if (height > maxHeight) {
+                maxHeight = height;
+            }
+            // Если все изображения загружены, устанавливаем высоту контейнера
+            if (imagesLoadedCount === objects.length) {
+                container.style.height = `${maxHeight}px`;
+            }
+        };
+    });
+    return container
+}
+
+function createNewSection(objects, index) {
+    const container = createNewContentContainer(objects, index)
+
+    const sectionDivider = document.createElement('img');
+    sectionDivider.className = index < 2 ? 'section-divider' : 'footer-image';
+    sectionDivider.alt = 'footer-image';
+    sectionDivider.src = index < 2 ? 'assets/images/section-divider.svg' : 'assets/images/footer-image.svg';
+
+    const highlightBar = document.createElement('div');
+    highlightBar.className = index < 2 ? 'highlight-bar' : 'gray-separator';
+
+    const section = document.createElement('div');
+    section.className = 'section'
+
+    section.appendChild(container)
+    section.appendChild(sectionDivider)
+    section.appendChild(highlightBar)
+
+    if (index < 2) {
+        const darkBar = document.createElement('div');
+        darkBar.className = 'dark-bar';
+        section.appendChild(darkBar)
+    }
+    // Позиционируем полку
+    if (index === 0) {
+        section.style.top = '0';
+    }
+    if (index === 1) {
+        section.style.top = '150px';
+    }
+    if (index === 2) {
+        section.style.top = '225px';
+    }
+    return section
+}
+
+function createBasketEvents() {
     // Получаем ссылку на корзину
     const basketGroup = document.querySelector('.basket-group');
 
@@ -194,6 +197,16 @@ function createDynamicHtml() {
         console.log('Кнопка была нажата.');
         window.location.href = 'https://lavka.yandex.ru';
     });
+}
+
+function createDynamicHtml() {
+    const productContainer = document.querySelector('.product-container')
+    objectsGroups.forEach((objects, index) => {
+        // Вставляем созданную секцию в тело документа
+        const section = createNewSection(objects, index)
+        productContainer.appendChild(section);
+    })
+    createBasketEvents()
 
     document.onselectstart = function() { return false; };
 }
